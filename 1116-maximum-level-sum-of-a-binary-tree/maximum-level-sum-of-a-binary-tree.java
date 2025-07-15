169ef44c5488355304801af6882b82292b411ca0
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2025-07-15 16:03:58 +0530

    Time: 9 ms (35.91%) | Memory: 47.4 MB (75.92%) - LeetSync

---
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        ArrayList<Integer> arr = new ArrayList<>();
+        if(root == null) return 1;
+        int i = 0;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        while(!q.isEmpty()){
+            int n = q.size();
+            int sum = 0;
+            while(n-- > 0){
+                TreeNode node = q.poll();
+                sum += node.val;
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+            i++;
+            mpp.put(sum, mpp.getOrDefault(sum,i));
+        }
+        int max = Integer.MIN_VALUE;
+        int max_level = 0;
+        for (Map.Entry<Integer, Integer> entry : mpp.entrySet()){
+            if(entry.getKey() > max){
+                max = entry.getKey();
+                max_level = entry.getValue();
+            }
+        }
+        return max_level;
+    }
+}
