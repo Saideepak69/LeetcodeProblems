a6f23f09bd1aadf0cc36880dccf7c6d973748539
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2025-09-04 19:05:18 +0530

    Time: 5 ms (38.12%) | Memory: 18 MB (95.33%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        mpp = {}
+        dups = []
+        minFreq = float('inf')
+        for card in deck:
+            mpp[card] = mpp.get(card, 0) + 1
+        for key, value in mpp.items():
+            if value in dups:
+                continue
+            dups.append([key, value])
+            minFreq = min(minFreq, value)
+        print(dups)
+        def gcd(a, b):
+            if b == 0:
+                return a
+            return gcd(b, a % b)
+        commonGcd = dups[0][1]
+        for i in range(1, len(dups)):
+            commonGcd = gcd(commonGcd, dups[i][1])
+        
+        return commonGcd > 1
