35a748779526b2be14a425ab324a6253d4d30ca9
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2025-11-17 07:57:10 +0530

    Time: 19 ms (6.86%) | Memory: 70.9 MB (5.71%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -5,11 +5,10 @@ class Solution {
         for(int i=0; i<n; i++){
             if(nums[i] == 1) arr.add(i);
         }
-        Collections.sort(arr);
-        System.out.println(arr);
-        for(int i=0; i<arr.size()-1; i++){
-            if(arr.get(i+1)-arr.get(i)-1 < k) return false;
+        for (int j = 0; j + 1 < arr.size(); j++) {
+            if(arr.get(j + 1)-arr.get(j)-1 < k) return false;
         }
+        System.out.println(arr);
         return true;
     }
 }
