bf9a96cd97c3219c972411074f8441001c74831a
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-01-31 23:22:43 +0530

    Time: 2 ms (2.06%) | Memory: 47.3 MB (9.86%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,14 +1,9 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        PriorityQueue<Character> pq = new PriorityQueue<>();
+        Arrays.sort(letters);
         for(char ch: letters){
-            if(ch > target){
-                pq.add(ch);
-            }
+            if(ch > target) return ch;
         }
-        if (pq.isEmpty()) {
-            return letters[0];
-        }
-        return pq.poll();
-    } 
+        return letters[0];
+    }
 }
