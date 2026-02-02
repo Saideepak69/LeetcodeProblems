78d4b0c852774691ebdb3a3c57abfebe08c93e7e
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-02-02 19:29:36 +0530

    Time: 23 ms (15.29%) | Memory: 63.8 MB (81.83%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -3,32 +3,17 @@ class Solution {
         Arrays.sort(arr);
         int n = arr.length;
         int minDiff = Integer.MAX_VALUE;
-        List<List<Integer>> ans = new ArrayList<>();
-        int fake1 = Integer.MAX_VALUE, fake2 = Integer.MAX_VALUE;
         for(int i=0; i<n-1; i++){
-            if(arr[i+1] - arr[i] == minDiff){
-                minDiff = arr[i+1] - arr[i];
-                fake1 = Math.min(fake1, arr[i]);
-                fake2 = Math.min(fake2, arr[i+1]);
-            }
-            else if (arr[i+1] - arr[i] < minDiff) {
-                minDiff = arr[i+1] - arr[i];
-                fake1 = arr[i];
-                fake2 = arr[i+1];
-            }
+            minDiff = Math.min(minDiff, arr[i+1] - arr[i]);
         }
-        List<Integer> fake = new ArrayList<>();
-        fake.add(fake1);
-        fake.add(fake2);
-        ans.add(fake);
-        for(int i=1; i<n-1; i++){
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<n-1; i++){
             List<Integer> res = new ArrayList<>();
             if(arr[i+1] - arr[i] == minDiff){
                 res.add(arr[i]);
                 res.add(arr[i+1]);
-                if(!ans.contains(res))
-                    ans.add(res);
-            } 
+                ans.add(res);
+            }
         }
         return ans;
     }
