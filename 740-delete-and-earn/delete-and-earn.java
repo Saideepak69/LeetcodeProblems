f26ac91b7922661061b8525e051f5fc0f3c2dab3
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-02-02 21:51:52 +0530

    Time: 12 ms (32.29%) | Memory: 46.3 MB (90.42%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,18 +1,16 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int max = 0;
+        Map<Integer,Integer> mpp = new HashMap<>();
         for(int n: nums){
             max = Math.max(max, n);
-        }
-        int[] map = new int[max+1];
-        for(int n: nums){
-            map[n] += n;
+            mpp.put(n, mpp.getOrDefault(n,0)+1);
         }
         int[] dp = new int[max+1];
-        dp[0] = map[0];
-        dp[1] = map[1];
+        dp[0] = mpp.getOrDefault(0, 0);
+        dp[1] = mpp.getOrDefault(1, 0);
         for(int i=2; i<=max; i++){
-            dp[i] = Math.max(map[i] + dp[i-2], dp[i-1]);
+            dp[i] = Math.max(i * mpp.getOrDefault(i, 0) + dp[i-2], dp[i-1]);
         }
         return dp[max];
     }
