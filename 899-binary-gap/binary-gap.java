b34162531e3ad71d2aed2e214300b797ce45bf54
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-02-22 15:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.49%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,20 +1,18 @@
 class Solution {
     public int binaryGap(int n) {
         ArrayList<Integer> arr = new ArrayList<>();
-        int index = 0;
+        int index = 0, prevInd = -1;
+        int maxGap = 0, oneCount = 0;
         while(n != 0){
-            if(n%2 == 1){
-                arr.add(index);
+            if ((n & 1) == 1) {
+                if (prevInd != -1) {
+                    maxGap = Math.max(maxGap, index - prevInd);
+                }
+                prevInd = index;
             }
             index++;
-            System.out.println(n%2);
-            n/=2;
+            n>>=1;
         }
-        int maxGap = 0;
-        for(int i=0; i<arr.size()-1; i++){
-            maxGap = Math.max(maxGap, arr.get(i+1) - arr.get(i));
-        }
-        System.out.println(arr);
         return maxGap;
     }
 }
