2f68a897f3b7a6c23b42a45159786d994c34a4d3
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-05-31 07:46:00 +0530

    Time: 26 ms (81.52%) | Memory: 107.2 MB (79.91%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        int n = asteroids.length;
+        long newMass = mass;
+        for(int i=0; i<n; i++){
+            if(newMass < asteroids[i]){
+                return false;
+            }
+            newMass += asteroids[i];
+        }
+        return true;
+    }
+}
