d1a2b3ba1b428f48ea1284ec5d9845738d0615f3
S Sai Deepak <157108476+Saideepak69@users.noreply.github.com>
2026-06-09 21:32:10 +0530

    Time: 1486 ms (40.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s.name
+FROM SalesPerson s
+LEFT JOIN Orders o ON s.sales_id = o.sales_id
+LEFT JOIN Company c ON o.com_id = c.com_id
+GROUP BY s.sales_id, s.name
+HAVING COUNT(IF(c.name = 'RED', 1, NULL)) = 0;
